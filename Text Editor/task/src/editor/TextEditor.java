c51fac8a2f6bbc015019d0efc11c113cedc9648c
frique92 <29735207+frique92@users.noreply.github.com>
2020-05-28 13:07:15 +0300

    complete 1 stage

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -6,7 +6,17 @@ public class TextEditor extends JFrame {
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
+        initComponent();
         setVisible(true);
         setLayout(null);
+    }
+
+    private void initComponent() {
+        setTitle("Text editor");
+
+        JTextArea textArea = new JTextArea();
+        textArea.setName("TextArea");
+
+        add(textArea);
     }
 }
