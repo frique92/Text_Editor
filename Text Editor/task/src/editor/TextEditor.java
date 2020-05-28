0ce13c69b8bf1efea43d0c6c71244f9477ac602d
frique92 <29735207+frique92@users.noreply.github.com>
2020-05-28 14:55:07 +0300

    complete 2 stage

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -1,12 +1,23 @@
 package editor;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class TextEditor extends JFrame {
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setSize(500, 500);
+
+        setLocationRelativeTo(null);
+
         initComponent();
+
         setVisible(true);
         setLayout(null);
     }
@@ -14,9 +25,91 @@ public class TextEditor extends JFrame {
     private void initComponent() {
         setTitle("Text editor");
 
-        JTextArea textArea = new JTextArea();
-        textArea.setName("TextArea");
+        //Menu
+        JMenuBar menuBar = new JMenuBar();
 
-        add(textArea);
+        JMenu menuFile = new JMenu("File");
+        menuFile.setName("MenuFile");
+        menuFile.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem menuSave = new JMenuItem("Save");
+        menuSave.setName("MenuSave");
+        menuFile.add(menuSave);
+
+        JMenuItem menuLoad = new JMenuItem("Load");
+        menuLoad.setName("MenuLoad");
+        menuFile.add(menuLoad);
+
+        JMenuItem menuExit = new JMenuItem("Exit");
+        menuExit.setName("MenuExit");
+        menuFile.addSeparator();
+        menuFile.add(menuExit);
+
+        menuBar.add(menuFile);
+        setJMenuBar(menuBar);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        JPanel panelSetting = new JPanel();
+        panelSetting.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+
+        JTextField textFieldFile = new JTextField();
+        textFieldFile.setName("FilenameField");
+        textFieldFile.setColumns(20);
+
+        JButton buttonSave = new JButton();
+        buttonSave.setName("SaveButton");
+        buttonSave.setText("Save");
+        buttonSave.setPreferredSize(new Dimension(80, 20));
+
+        JButton buttonLoad = new JButton();
+        buttonLoad.setName("LoadButton");
+        buttonLoad.setText("Load");
+        buttonLoad.setPreferredSize(new Dimension(80, 20));
+
+        panelSetting.add(textFieldFile);
+        panelSetting.add(buttonSave);
+        panelSetting.add(buttonLoad);
+
+        JTextArea textAreaText = new JTextArea();
+        textAreaText.setName("TextArea");
+
+        JScrollPane jScrollPaneText = new JScrollPane(textAreaText);
+        jScrollPaneText.setName("ScrollPane");
+
+        mainPanel.add(panelSetting);
+        mainPanel.add(textAreaText);
+        mainPanel.add(jScrollPaneText);
+
+        add(mainPanel, BorderLayout.CENTER);
+
+        buttonSave.addActionListener(e -> saveFile(textFieldFile.getText(), textAreaText.getText()));
+        menuSave.addActionListener(e -> saveFile(textFieldFile.getText(), textAreaText.getText()));
+        buttonLoad.addActionListener(e -> textAreaText.setText(loadFile(textFieldFile.getText())));
+        menuLoad.addActionListener(e -> textAreaText.setText(loadFile(textFieldFile.getText())));
+        menuExit.addActionListener(e -> System.exit(0));
+
+
+    }
+
+
+    private String loadFile(String pathToFile) {
+
+        try {
+            return new String(Files.readAllBytes(Paths.get(pathToFile)));
+        } catch(IOException e) {
+            System.out.printf("An exception occurs %s", e.getMessage());
+            return "";
+        }
+    }
+
+    private void saveFile(String pathToFile, String text) {
+
+        try (FileWriter writer = new FileWriter(pathToFile)) {
+            writer.write(text);
+        } catch (IOException e) {
+            System.out.printf("An exception occurs %s", e.getMessage());
+        }
     }
 }
